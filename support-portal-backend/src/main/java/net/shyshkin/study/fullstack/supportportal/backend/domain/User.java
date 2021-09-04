d669d8345d611ecb346a4bd357cd4700b7028f50
Art <d.art.shishkin@gmail.com>
2021-09-04 08:33:11 +0300

    15. User domain - part 2 (#1)

--- support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/domain/User.java
+++ support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/domain/User.java
@@ -1,14 +1,32 @@
 package net.shyshkin.study.fullstack.supportportal.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User implements Serializable {
 
     private static final long serialVersionUID = -4372214856545239049L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
     private String userId;
     private String firstName;
     private String lastName;
