15421bad878f3805dcd6de3c98e2028683695e4d
Art <d.art.shishkin@gmail.com>
2021-09-05 14:43:38 +0300

    33. User Repository (#2)

--- support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/repository/UserRepository.java
+++ support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/repository/UserRepository.java
@@ -3,5 +3,12 @@ package net.shyshkin.study.fullstack.supportportal.backend.repository;
 import net.shyshkin.study.fullstack.supportportal.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findAllByEmail(String email);
+
 }
