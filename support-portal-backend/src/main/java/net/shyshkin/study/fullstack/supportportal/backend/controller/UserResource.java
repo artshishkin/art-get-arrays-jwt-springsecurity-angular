25a608615fdc59ce2cd94a08e3c3b64785c50089
Art <d.art.shishkin@gmail.com>
2021-09-07 14:59:13 +0300

    49. User registration - Register User Endpoint (#4)

--- support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/controller/UserResource.java
+++ support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/controller/UserResource.java
@@ -1,15 +1,24 @@
 package net.shyshkin.study.fullstack.supportportal.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.fullstack.supportportal.backend.domain.User;
+import net.shyshkin.study.fullstack.supportportal.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
+@RequiredArgsConstructor
 public class UserResource {
+
+    private final UserService userService;
 
     @GetMapping("home")
     public String showUser() {
         return "Application works";
+    }
+
+    @PostMapping("register")
+    public User register(@RequestBody User user) {
+        return userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
     }
 }
