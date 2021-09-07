35752452386a12031ae4ff7ef7c0ff70e0a5a48d
Art <d.art.shishkin@gmail.com>
2021-09-07 10:14:22 +0300

    46. User registration - Interface (#4)

--- support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/service/UserService.java
+++ support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/service/UserService.java
@@ -1,6 +1,18 @@
 package net.shyshkin.study.fullstack.supportportal.backend.service;
 
+import net.shyshkin.study.fullstack.supportportal.backend.domain.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
+
+    User register(String firstName, String lastName, String username, String email);
+
+    List<User> findAll();
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
 }
