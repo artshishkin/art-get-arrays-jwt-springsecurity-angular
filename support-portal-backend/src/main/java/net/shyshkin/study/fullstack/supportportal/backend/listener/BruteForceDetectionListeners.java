65f971c326227434f876f1f4634a3466e72eb61b
Art <d.art.shishkin@gmail.com>
2021-09-08 16:49:59 +0300

    Refactored Brute Force Detection Listeners (#6)

--- support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/listener/BruteForceDetectionListeners.java
+++ support-portal-backend/src/main/java/net/shyshkin/study/fullstack/supportportal/backend/listener/BruteForceDetectionListeners.java
@@ -3,7 +3,8 @@ package net.shyshkin.study.fullstack.supportportal.backend.listener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.fullstack.supportportal.backend.service.LoginAttemptService;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -11,12 +12,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class AuthenticationSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
+public class BruteForceDetectionListeners  {
 
     private final LoginAttemptService loginAttemptService;
 
-    @Override
-    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+    @EventListener
+    public void onLoginFailure(AuthenticationFailureBadCredentialsEvent event) {
+        String username = event.getAuthentication().getPrincipal().toString();
+        log.debug("{} failed to login", username);
+        loginAttemptService.loginFailed(username);
+    }
+
+    @EventListener
+    public void onLoginSuccess(AuthenticationSuccessEvent event) {
         Object principal = event.getAuthentication().getPrincipal();
         if (principal instanceof UserDetails) {
             String username = ((UserDetails) principal).getUsername();
