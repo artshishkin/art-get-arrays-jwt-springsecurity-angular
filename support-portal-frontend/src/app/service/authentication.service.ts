fa5c69952cc380f540b6583385cab60feb5f3607
Art <d.art.shishkin@gmail.com>
2021-09-12 11:33:40 +0300

    98. Login service call (#12)

--- support-portal-frontend/src/app/service/authentication.service.ts
+++ support-portal-frontend/src/app/service/authentication.service.ts
@@ -1,9 +1,22 @@
 import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {UserLogin} from "../dto/user-login";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private host: string = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public login(userDto: UserLogin): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.httpClient.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, userDto, {observe: 'response'});
+  }
+
+
 }
