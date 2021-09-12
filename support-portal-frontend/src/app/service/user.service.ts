fbd3b08432f0d4e2b46e885f3e1edfc7d6cf3564
Art <d.art.shishkin@gmail.com>
2021-09-12 14:09:42 +0300

    108. User list service call (#13 Section 15: User service)

--- support-portal-frontend/src/app/service/user.service.ts
+++ support-portal-frontend/src/app/service/user.service.ts
@@ -1,9 +1,34 @@
 import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private host: string = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getAllUsers(): Observable<UserPage | HttpErrorResponse> {
+    return this.httpClient
+      .get<UserPage>(`${this.host}/user`);
+  }
+}
+
+export interface UserPage {
+
+  content: User[];
+  last: boolean;
+  first: boolean;
+  totalElements: number;
+  size: number;
+  numberOfElements: number;
+  number: number;
+  empty: boolean;
+
 }
