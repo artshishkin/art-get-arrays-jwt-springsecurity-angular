f77504073878af108bc7232f51786df4e23bcee9
Art <d.art.shishkin@gmail.com>
2021-09-13 21:00:22 +0300

    122. Configure HTTP Interceptor (#14)

--- support-portal-frontend/src/app/app.module.ts
+++ support-portal-frontend/src/app/app.module.ts
@@ -2,7 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AuthenticationService} from "./service/authentication.service";
+import {UserService} from "./service/user.service";
+import {AuthInterceptor} from "./interceptor/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService, UserService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
