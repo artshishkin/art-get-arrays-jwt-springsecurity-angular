7aedfe5ea798ea0c539b2f6dea783fef9d281796
Art <d.art.shishkin@gmail.com>
2021-09-17 10:06:57 +0300

    126. Configure Guard (#15)

--- support-portal-frontend/src/app/app.module.ts
+++ support-portal-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthenticationService} from "./service/authentication.service";
 import {UserService} from "./service/user.service";
 import {AuthInterceptor} from "./interceptor/auth.interceptor";
+import {AuthenticationGuard} from "./guard/authentication.guard";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import {AuthInterceptor} from "./interceptor/auth.interceptor";
     BrowserModule,
     HttpClientModule
   ],
-  providers: [AuthenticationService, UserService,
+  providers: [AuthenticationGuard, AuthenticationService, UserService,
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
