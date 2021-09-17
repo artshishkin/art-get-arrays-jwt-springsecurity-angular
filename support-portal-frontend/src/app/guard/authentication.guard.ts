2fb286ac0004a00c37df9795ec5d425e0a166c1a
Art <d.art.shishkin@gmail.com>
2021-09-17 10:05:16 +0300

    125. Creating Guard (#15)

--- support-portal-frontend/src/app/guard/authentication.guard.ts
+++ support-portal-frontend/src/app/guard/authentication.guard.ts
@@ -1,15 +1,31 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
+import {AuthenticationService} from "../service/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.isUserLoggedIn();
   }
 
+  private isUserLoggedIn(): boolean {
+    if( this.authenticationService.isUserLoggedIn())
+      return true;
+    else {
+      this.router.navigate(['/login']);
+
+      //todo - send notification to user
+
+      return false;
+    }
+  }
 }
