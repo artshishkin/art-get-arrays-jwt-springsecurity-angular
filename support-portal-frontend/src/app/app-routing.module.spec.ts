5e1159711b33c0b3c95b53e90deedb538d6db552
Art <d.art.shishkin@gmail.com>
2021-09-17 17:32:46 +0300

    135. Test routes (#17)

---
+++ support-portal-frontend/src/app/app-routing.module.spec.ts
@@ -0,0 +1,24 @@
+import {routes} from './app-routing.module';
+import {UserComponent} from "./component/user/user.component";
+import {LoginComponent} from "./component/login/login.component";
+import {RegisterComponent} from "./component/register/register.component";
+
+describe('routes', () => {
+
+  it('should contain a route for /login', () => {
+    expect(routes).toContain({path: "login", component: LoginComponent});
+  });
+
+  it('should contain a route for /register', () => {
+    expect(routes).toContain({path: "register", component: RegisterComponent});
+  });
+
+  it('should contain a route for /user/management', () => {
+    expect(routes).toContain({path: "user/management", component: UserComponent});
+  });
+
+  it('should redirect to /login endpoint if no route found', () => {
+    expect(routes).toContain({path: '', redirectTo: '/login', pathMatch: 'full'});
+  });
+
+})
--- support-portal-frontend/src/app/app-routing.module.ts
+++ support-portal-frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {LoginComponent} from "./component/login/login.component";
 import {RegisterComponent} from "./component/register/register.component";
 import {UserComponent} from "./component/user/user.component";
 
-const routes: Routes = [
+export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'user/management', component: UserComponent},
