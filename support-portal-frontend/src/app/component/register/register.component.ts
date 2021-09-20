b7f4b8f965d54dba505264d198704b31e6c386db
Art <d.art.shishkin@gmail.com>
2021-09-20 12:00:07 +0300

    152. Register template (#20 Section 21: Register Page)

--- support-portal-frontend/src/app/component/register/register.component.ts
+++ support-portal-frontend/src/app/component/register/register.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public showLoading: boolean;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  public onRegister(user: User) {
+
   }
 
 }
