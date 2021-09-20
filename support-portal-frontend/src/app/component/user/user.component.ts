c0ce9d48633caa2705f7e214f019ead08da25649
Art <d.art.shishkin@gmail.com>
2021-09-20 16:17:51 +0300

    156. Adding tab name - Part 1 - BehaviorSubject (#21)

--- support-portal-frontend/src/app/component/user/user.component.ts
+++ support-portal-frontend/src/app/component/user/user.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  private titleSubject = new BehaviorSubject<string>('Users');
+  public titleAction$ = this.titleSubject.asObservable();
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  public changeTitle(title: string): void {
+    this.titleSubject.next(title);
   }
 
 }
