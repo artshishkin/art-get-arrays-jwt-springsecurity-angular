b11ede8e54a75728e060ddea4cc50fc15a469333
Art <d.art.shishkin@gmail.com>
2021-10-08 13:37:38 +0300

    31.3. Management Component Base Implementation (#31)

--- support-portal-frontend/src/app/component/management/management.component.ts
+++ support-portal-frontend/src/app/component/management/management.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {Role} from "../../enum/role.enum";
+import {User} from "../../model/user";
+import {AuthenticationService} from "../../service/authentication.service";
+import {Router} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-management',
@@ -7,9 +12,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ManagementComponent implements OnInit {
 
-  constructor() { }
+  public loggedInUser: User;
+  private titleSubject = new BehaviorSubject<string>('Users');
+  public titleAction$ = this.titleSubject.asObservable();
+
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.loggedInUser = this.authenticationService.getUserFromLocalStorage();
   }
 
+  public changeTitle(title: string): void {
+    this.titleSubject.next(title);
+  }
+
+  public get isAdmin(): boolean {
+    return this.loggedInUser.role === Role.ADMIN || this.loggedInUser.role === Role.SUPER_ADMIN;
+  }
+
+  public get isManager(): boolean {
+    return this.isAdmin || this.loggedInUser.role === Role.MANAGER;
+  }
 }
