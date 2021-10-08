822d20aa008a1a0f08ee63d09e3e25ef7dbe671d
Art <d.art.shishkin@gmail.com>
2021-10-08 11:50:29 +0300

    31.2. Configure routing (#31)

--- support-portal-frontend/src/app/app-routing.module.ts
+++ support-portal-frontend/src/app/app-routing.module.ts
@@ -4,11 +4,31 @@ import {LoginComponent} from "./component/login/login.component";
 import {RegisterComponent} from "./component/register/register.component";
 import {UserComponent} from "./component/user/user.component";
 import {AuthenticationGuard} from "./guard/authentication.guard";
+import {ManagementComponent} from "./component/management/management.component";
+import {UsersComponent} from "./component/management/users/users.component";
+import {SettingsComponent} from "./component/management/settings/settings.component";
+import {ProfileComponent} from "./component/management/profile/profile.component";
+import {UserEditComponent} from "./component/management/users/user-edit/user-edit.component";
+import {UserViewComponent} from "./component/management/users/user-view/user-view.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'user/management', component: UserComponent, canActivate: [AuthenticationGuard]},
+  {
+    path: 'management', component: ManagementComponent, canActivate: [AuthenticationGuard],
+    children: [
+      {path: 'settings', component: SettingsComponent},
+      {path: 'profile', component: ProfileComponent},
+      {
+        path: 'users', component: UsersComponent,
+        children: [
+          {path: ':id/view', component: UserViewComponent},
+          {path: ':id/edit', component: UserEditComponent}
+        ]
+      }
+    ]
+  },
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
